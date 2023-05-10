f6334306804b7bd975424b385994f2fe58812986
subhadippaul2311 <126654387+subhadippaul2311@users.noreply.github.com>
2023-05-10 09:47:24 +0530

    Css Applied

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import Button from './Button';
+import Button from './Button'; 
+import './style.css'
 
 export default function App() {
   const handleClick = () => {
     alert('clicked');
   };
   return (
-    <div>
+    <div className="button">
       <Button onClick={handleClick} />
     </div>
   );
